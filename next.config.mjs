674468b9ddc3bf86b5b559733e58a39949d27765
s23022 <s23022@std.it-collegel.ac.jp>
2025-10-14 15:36:54 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import nextPWA from "next-pwa";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isMobile = process.env.NEXT_PUBLIC_IS_MOBILE === 'true';
+const withPWA = nextPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+})
+const nextConfig = withPWA({
+    reactStrictMode: true,
+});
 
 export default nextConfig;
