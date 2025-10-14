97430a2094ac2cd51f22ed5d0dab6d3a716215db
s23022 <s23022@std.it-collegel.ac.jp>
2025-10-14 16:06:11 +0900

    first commit

--- app/page.js
+++ app/page.js
@@ -2,8 +2,10 @@ import styles from './page.module.css';
 
 export default function Home() {
     return (
-        <main className={styles.main}>{/* スマホのサイズに設定 */}
+        <div className={styles.body}>
+        <div className={styles.main}>{/* スマホのサイズに設定 */}
             <p className={styles.text}>hello</p> {/* とりあえず配置したテキスト、上下左右スマホのサイズになったら真ん中に来るようCSS書いてる */}
-        </main>
+        </div>
+        </div>
     )
 }
