4b2eb295a1729e92c130d9c82744426a4fd6e93c
s23022 <s23022@std.it-collegel.ac.jp>
2025-10-21 10:04:11 +0900

    test

--- app/page.js
+++ app/page.js
@@ -2,10 +2,8 @@ import styles from './page.module.css';
 
 export default function Home() {
     return (
-        <div className={styles.body}>
-            <div className={styles.main}>{/* スマホのサイズに設定 */}
-                <p className={styles.text}>hello</p> {/* とりあえず配置したテキスト、上下左右スマホのサイズになったら真ん中に来るようCSS書いてる */}
-            </div>
-        </div>
+        <main className={styles.main}>{/* スマホのサイズに設定 */}
+            <p className={styles.text}>hello</p> {/* とりあえず配置したテキスト、上下左右スマホのサイズになったら真ん中に来るようCSS書いてる */}
+        </main>
     )
 }
