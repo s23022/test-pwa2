ff9c5a265d5b64afecf4b80f1ecf28177cfe0d52
s23022 <s23022@std.it-collegel.ac.jp>
2025-10-21 12:34:29 +0900

    ホーム画面のナビゲーションバーの作成、とりあえず外観だけ

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
 import styles from './page.module.css';
 
+
 export default function Home() {
     return (
         <main className={styles.main}>{/* スマホのサイズに設定 */}
-            <p className={styles.text}>hello</p> {/* とりあえず配置したテキスト、上下左右スマホのサイズになったら真ん中に来るようCSS書いてる */}
+
+            <p className={styles.text}>hello world</p> {/* とりあえず配置したテキスト、上下左右スマホのサイズになったら真ん中に来るようCSS書いてる */}
+            <div className={styles.nav}>
+                <button className={styles.button01}>
+                    <img className={styles.home_img03} src="/images/recode.png" alt="記録ボタン用の画像"/>
+                </button>
+                <button className={styles.button02}>
+                    <img className={styles.home_img02} src="/images/run_human.png" alt="ホームボタン用の画像"/>
+                </button>
+                <button className={styles.button03}>
+                    <img className={styles.home_img03} src="/images/ranking.png" alt="ランキングボタン用の画像"/>
+                </button>
+            </div>
         </main>
     )
 }
